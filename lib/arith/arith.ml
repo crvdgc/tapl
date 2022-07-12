c44f565f07a0e6224d7e554ece1ae114fbbbcd22
crvdgc <ubikium@gmail.com>
2022-07-13 00:15:07 +0900

    add eval

--- lib/arith/arith.ml
+++ lib/arith/arith.ml
@@ -36,3 +36,89 @@ let rec is_numeric_val = function
       is_numeric_val t
   | _ ->
       false
+
+
+let is_val = function
+  | TmTrue | TmFalse ->
+      true
+  | t when is_numeric_val t ->
+      true
+  | _ ->
+      false
+
+
+module type Eval = sig
+  exception NoRuleApplies
+
+  val eval : term -> term
+end
+
+exception NoRuleApplies
+
+let rec eval1 = function
+  | TmIf t when t.cond = TmTrue ->
+      t.tbranch
+  | TmIf t when t.cond = TmFalse ->
+      t.fbranch
+  | TmIf t ->
+      TmIf { t with cond = eval1 t.cond }
+  | TmSucc t ->
+      TmSucc (eval1 t)
+  | TmPred TmZero ->
+      TmZero
+  | TmPred (TmSucc nv) when is_numeric_val nv ->
+      nv
+  | TmPred t ->
+      TmPred (eval1 t)
+  | TmIsZero TmZero ->
+      TmTrue
+  | TmIsZero (TmSucc nv) when is_numeric_val nv ->
+      TmFalse
+  | TmIsZero t ->
+      TmIsZero (eval1 t)
+  | _ ->
+      raise NoRuleApplies
+
+
+module SmallStep : Eval = struct
+  exception NoRuleApplies = NoRuleApplies
+
+  let eval = eval1
+end
+
+let rec eval_many = function
+  | TmIf t ->
+    ( match eval_many t.cond with
+    | TmTrue ->
+        eval_many t.tbranch
+    | TmFalse ->
+        eval_many t.fbranch
+    | _ ->
+        raise NoRuleApplies )
+  | TmSucc t ->
+      TmSucc (eval_many t)
+  | TmPred t ->
+    ( match eval_many t with
+    | TmZero ->
+        TmZero
+    | TmSucc nv when is_numeric_val nv ->
+        nv
+    | _ ->
+        raise NoRuleApplies )
+  | TmIsZero t ->
+    ( match eval_many t with
+    | TmZero ->
+        TmTrue
+    | TmSucc nv when is_numeric_val nv ->
+        TmFalse
+    | _ ->
+        raise NoRuleApplies )
+  | _ ->
+      raise NoRuleApplies
+
+
+module BigStep : Eval = struct
+  exception NoRuleApplies = NoRuleApplies
+
+  let eval = eval_many
+end
--- lib/arith/arith.mli
+++ lib/arith/arith.mli
@@ -18,3 +18,17 @@ val size : term -> int
 val depth : term -> int
 
 val is_numeric_val : term -> bool
+
+val is_val : term -> bool
+
+exception NoRuleApplies
+
+val eval1 : term -> term
+
+module type Eval = sig
+  exception NoRuleApplies
+
+  val eval : term -> term
+end
+
+module SmallStep : Eval
